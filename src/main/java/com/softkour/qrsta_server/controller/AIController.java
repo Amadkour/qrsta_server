e55f1d5f9c8649be2f0ebab57e5a526535ddb2ac
Ahmed <amira.gomaa@res.inc>
2024-01-25 14:08:27 +0200

    AI

---
+++ src/main/java/com/softkour/qrsta_server/controller/AIController.java
@@ -0,0 +1,18 @@
+package com.softkour.qrsta_server.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * AIController
+ */
+@RestController
+public class AIController {
+
+    /// ============[ analysis scheduals]===============///
+    /// ============[ analysis income students]=========///
+    /// ============[ analysis missed students]=========///
+    /// ============[ analysis income money]==========///
+    /// ============[ analysis quizzes ]==========///
+    /// ============[ analysis objectives ]==========///
+
+}
