b9b9d4fd06ac2f4af6af996c5425f4385dd2f16b
Ahmed Madkour <ahmed@vega4it.com>
2024-02-02 23:04:09 +0200

    student

--- src/main/java/com/softkour/qrsta_server/repo/UserRepository.java
+++ src/main/java/com/softkour/qrsta_server/repo/UserRepository.java
@@ -3,11 +3,12 @@ package com.softkour.qrsta_server.repo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import com.softkour.qrsta_server.entity.user.User;
 
-@Repository
+@EnableJpaRepositories
 public interface UserRepository extends JpaRepository<User, Long> {
 
    public User findUserByPhoneNumber(String phone);
